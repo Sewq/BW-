3e5645ee00b5f11e8f83eea6d0b2280e612a6a7d
Michael Wojtas <ext-michael.wojtas@omikron.net>
2020-11-20 15:55:30 +0100

    added some things

---
+++ src/Items.js
@@ -0,0 +1,19 @@
+export let headPrefixes = [
+    {name: 'Bez glowy', dex: 0, res: 0, def: 0, id: -1},
+    {name: 'Tygrysia', dex: 34, res: 12, def: -12, id: 1},
+    {name: 'Runiczna', luck: 12, def: 37, hardness: 3, id: 2}
+];
+
+export let headMainPart = [
+    {name: 'Bez glowy', ignor: 0, spost: 0, look: 0, def: 0},
+    {name: 'Opaska', ignor: 0.2, spost: 26, look: 32, def: 28},
+    {name: 'Bandana', minDmg: 7, maxDmg: 10, spost: 4, dmg: '1 / 4', def: 37},
+    {name: 'Korona', char: 26, wpl: 26, def: '1 / 4', hardness: 4}
+];
+
+export let headSufixes = [
+    {name: 'Bez glowy', ignor: 0, spost: 0, look: 0, def: 0},
+    {name: 'Prekognicji', spost: 42},
+    {name: 'Adrenaliny', str: 34, spost: -12, dex: 31},
+    {name: 'Magii', bloodMod: -5, luck: 4, def: 4 / 4, hardness: 6}
+];
--- src/Stat.js
+++ src/Stat.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {headPrefixes, headSufixes, headMainPart} from './Items'
 
 function Stat() {
 
     const [playerDex, setPlayerDex] = useState(0);
+    const [playerLevel, setPlayerLevel] = useState(0);
     const [playerSpost, setPlayerSpost] = useState(0);
     const [str, setStr] = useState(0);
     const [def, setDef] = useState(0);
@@ -13,6 +15,10 @@ function Stat() {
     const [wis, setWis] = useState(0);
     const [luck, setLuck] = useState(0);
     const [baseHp, setBaseHp] = useState(0);
+    const [blood, setBlood] = useState(0);
+    const [calculatetBaseHp, setcalculatedBaseHp] = useState(0);
+
+    const [evoLevels, setEvoLevels] = useState(0);
 
 
     const [finalDex, setFinalDex] = useState(0);
@@ -32,6 +38,19 @@ function Stat() {
     const [race, setRace] = useState({luck: 0, hpMod: 0});
 
 
+    const [firstWeapon, setFirstWeapon] = useState({hpMod: 0, luck: 0});
+    const [secondWeapon, setSecondWeapon] = useState({luck: 0, hpMod: 0});
+    const [firstRing, setfirstRing] = useState({luck: 0, hpMod: 0});
+    const [secondRing, setSecondRing] = useState({luck: 0, hpMod: 0});
+
+    const [headPref, setHeadPref] = useState({luck: 0, hpMod: 0, spost: 0, dex: 0});
+    const [headSuf, setHeadSuf] = useState({luck: 0, hpMod: 0, spost: 0, dex: 0});
+    const [headMain, setHeadMain] = useState({luck: 0, hpMod: 0, spost: 0, dex: 0});
+
+
+    const [chest, setChest] = useState({luck: 0, hpMod: 0});
+    const [legs, setLegs] = useState({luck: 0, hpMod: 0});
+
     const handleFocus = (event) => event.target.select();
 
     const missingLevelsPercent = ((maxLevels - levels) / maxLevels);
@@ -44,13 +63,6 @@ function Stat() {
     const suma = (70 + (3 * (parseInt(bossDex) + parseInt(bossSpost))) + parseInt(additionalToHit));
     const sumaHit = suma - (3 * (parseInt(playerDex) + parseInt(playerSpost)));
 
-    function behe(e, multiplier) {
-
-        setbeheDex(dexFromBehe);
-        setbeheSpost(spostFromBehe);
-        setFinalDex(parseInt(playerDex) + parseInt(beheDex));
-        setFinalSpost(parseInt(playerSpost) + parseInt(beheSpost));
-    }
 
     const dexFromBehe = (parseInt(str) * beheMultiplier);
     const spostFromBehe = (parseInt(int) + parseInt(wis)) * beheMultiplier;
@@ -60,12 +72,19 @@ function Stat() {
 
     const setTalismanSpost = (spostFromTalisman) => setSpostFromTalisman(spostFromTalisman);
 
+    const onChange = selectedOption => setFirstWeapon(JSON.parse(selectedOption.target.value));
+    const onHeadPrefChange = selectedOption => setHeadPref(JSON.parse(selectedOption.target.value));
+    const onHeadMainChange = selectedOption => setHeadMain(JSON.parse(selectedOption.target.value));
+    const onHeadSufChange = selectedOption => setHeadSuf(JSON.parse(selectedOption.target.value));
+
 
     return (
         <>
             <p>
 
-                final dex: {parseInt(playerDex) + parseInt(dexFromBehe) + parseInt(dexFromTalisman)}
+                final
+                dex: {(parseInt(playerDex) || 0) + (parseInt(dexFromBehe) || 0) + (parseInt(dexFromTalisman) || 0) + (parseInt(headMain.dex) || 0) + (parseInt(headPref.dex) || 0) + (parseInt(headSuf.dex) || 0)}
+
             </p>
             <p>
 
@@ -73,13 +92,51 @@ function Stat() {
             </p>
             <p>
 
-                final hp: {Math.ceil(parseInt(baseHp) + (parseInt(baseHp) * race.hpMod))}
+                final hp: {Math.ceil(parseInt(baseHp) + (parseInt(baseHp) * race.hpMod) + (parseInt(baseHp) * firstWeapon.hpMod))}
             </p>
             <p>
 
                 final luck: {parseInt(luck) + race.luck}
             </p>
+            <div>
+                Moj level:
+                <input
+                    type="text"
+                    style={{marginLeft: 10}}
+                    placeholder="Type stat..."
+                    value={playerLevel}
+                    name="filterText"
+                    onFocus={handleFocus}
+                    onChange={(e) => {
+                        setPlayerLevel(e.target.value);
+                        setEvoLevels((parseInt(e.target.value) - 65));
 
+                        let hpBase = 0;
+
+                        let s;
+                        for (s = 0; s <= levels; s++) {
+                            hpBase = hpBase + 4 + (s / 10);
+                            if (s % 10 === 0) {
+                                hpBase = hpBase + (s - 1);
+                            }
+                        }
+                        setcalculatedBaseHp(hpBase);
+                    }}
+                />
+                <label style={{paddingLeft: 30}}>Wolne punkty evo: {evoLevels}</label>
+            </div>
+            <div>
+                Moja krew:
+                <input
+                    type="text"
+                    style={{marginLeft: 10}}
+                    placeholder="Type stat..."
+                    value={blood}
+                    name="filterText"
+                    onFocus={handleFocus}
+                    onChange={(e) => setBlood(e.target.value)}
+                />
+            </div>
             <div>
                 Base hp:
                 <input
@@ -91,6 +148,10 @@ function Stat() {
                     onFocus={handleFocus}
                     onChange={(e) => setBaseHp(e.target.value)}
                 />
+                <label style={{paddingLeft: 20}}>
+                    Modyfikator z broni: {firstWeapon.hpMod} %
+                </label>
+
             </div>
             <div>
                 Moj dex:
@@ -230,6 +291,217 @@ function Stat() {
                     <input onClick={() => setTalismanSpost(0)} type="radio" value="Spost lv 0" name="Spost"/> Spost lv 0
                     <input onClick={() => setTalismanSpost(35)} type="radio" value="Spost lv 3" name="Spost"/> Spost lv 3
                     <input onClick={() => setTalismanSpost(50)} type="radio" value="Spost lv 4" name="Spost"/> Spost lv 4
+                </div>
+            </p>
+            <p>
+                <div>
+                    <label style={{paddingRight: 20}} htmlFor="pref">Palec 1:</label>
+                    <select id="pref">
+                        <option value={{}} onChange={() => {
+                        }}>Tanczacy
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>Msciwy
+                        </option>
+                    </select>
+                    <select id="base">
+                        <option value={{}} onChange={() => {
+                        }}>Krawat
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>Naszyjnik
+                        </option>
+                    </select>
+                    <select id="suf">
+                        <option value={{}} onChange={() => {
+                        }}>gowna
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>dupy
+                        </option>
+                    </select>
+
+                </div>
+                <div>
+                    <label style={{paddingRight: 20}} htmlFor="pref">Palec 2:</label>
+                    <select id="q">
+                        <option value={{}} onChange={() => {
+                        }}>Tanczacy
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>Msciwy
+                        </option>
+                    </select>
+                    <select id="w">
+                        <option value={{}} onChange={() => {
+                        }}>Krawat
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>Naszyjnik
+                        </option>
+                    </select>
+                    <select id="e">
+                        <option value={{}} onChange={() => {
+                        }}>gowna
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>dupy
+                        </option>
+                    </select>
+
+                </div>
+                <div>
+                    <label style={{paddingRight: 20}} htmlFor="pref">Szyja:</label>
+                    <select id="r">
+                        <option value={{}} onChange={() => {
+                        }}>Tanczacy
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>Msciwy
+                        </option>
+                    </select>
+                    <select id="t">
+                        <option value={{}} onChange={() => {
+                        }}>Krawat
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>Naszyjnik
+                        </option>
+                    </select>
+                    <select id="y">
+                        <option value={{}} onChange={() => {
+                        }}>gowna
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>dupy
+                        </option>
+                    </select>
+
+                </div>
+                <div>
+                    <label style={{paddingRight: 20}} htmlFor="pref">Bron 1:</label>
+                    <select id="u" onChange={onChange}>
+                        <option value={JSON.stringify({hpMod: 0, luck: 0})}>Bez broni</option>
+                        <option value={JSON.stringify({hpMod: 0.49, luck: 12})}>Opiekunczy</option>
+                        <option value={JSON.stringify({hpMod: 0.19, luck: 8})}>Msciwy</option>
+                    </select>
+                    <select id="i">
+                        <option value={{}} onChange={() => {
+                        }}>Waki
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>Naszyjnik
+                        </option>
+                    </select>
+                    <select id="o">
+                        <option value={{}} onChange={() => {
+                        }}>gowna
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>dupy
+                        </option>
+                    </select>
+                </div>
+                <div>
+                    <label style={{paddingRight: 20}} htmlFor="pref">Bron 2:</label>
+                    <select id="a">
+                        <option value={{}} onChange={() => {
+                        }}>Tanczacy
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>Msciwy
+                        </option>
+                    </select>
+                    <select id="s">
+                        <option value={{}} onChange={() => {
+                        }}>Krawat
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>Naszyjnik
+                        </option>
+                    </select>
+                    <select id="d">
+                        <option value={{}} onChange={() => {
+                        }}>gowna
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>dupy
+                        </option>
+                    </select>
+                </div>
+                <div>
+                    <label style={{paddingRight: 20}} htmlFor="pref">Glowa:</label>
+
+                    <select id="headSuf" onChange={onHeadPrefChange}>
+                        {headPrefixes.map((option) => (
+                            <option key={option.name} value={JSON.stringify(option)}>{option.name}</option>
+                        ))}
+                    </select>
+                    <select id="headMain" onChange={onHeadMainChange}>
+                        {headMainPart.map((option) => (
+                            <option key={option.name} value={JSON.stringify(option)}>{option.name}</option>
+                        ))}
+                    </select>
+                    <select id="headSuf" onChange={onHeadSufChange}>
+                        {headSufixes.map((option) => (
+                            <option key={option.name} value={JSON.stringify(option)}>{option.name}</option>
+                        ))}
+
+                    </select>
+                </div>
+                <div>
+                    <label style={{paddingRight: 20}} htmlFor="pref">Klata:</label>
+                    <select id="j">
+                        <option value={{}} onChange={() => {
+                        }}>Tanczacy
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>Msciwy
+                        </option>
+                    </select>
+                    <select id="k">
+                        <option value={{}} onChange={() => {
+                        }}>Krawat
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>Naszyjnik
+                        </option>
+                    </select>
+                    <select id="l">
+                        <option value={{}} onChange={() => {
+                        }}>gowna
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>dupy
+                        </option>
+                    </select>
+                </div>
+                <div>
+                    <label style={{paddingRight: 20}} htmlFor="pref">Nogi:</label>
+                    <select id="z">
+                        <option value={{}} onChange={() => {
+                        }}>Tanczacy
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>Msciwy
+                        </option>
+                    </select>
+                    <select id="x">
+                        <option value={{}} onChange={() => {
+                        }}>Krawat
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>Naszyjnik
+                        </option>
+                    </select>
+                    <select id="c">
+                        <option value={{}} onChange={() => {
+                        }}>gowna
+                        </option>
+                        <option value={{}} onChange={() => {
+                        }}>dupy
+                        </option>
+                    </select>
                 </div>
             </p>
             <hr/>
